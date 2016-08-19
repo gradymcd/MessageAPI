fb67fe5722229c7fbfc6fbe76e8197c3dd9b498d
Grady McDowell <gradyrmcd@gmail.com>
2016-08-19 13:59:54 -0400

    Allow changing user preferences

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,7 @@
 var User = new require('../models/User');
 var response = new require('./response');
 
+// This is very insecure and should never be used except for testing
 exports.post_users = function (req, res) {
 	var user = new User({
 		username: req.body.username,
@@ -19,9 +20,24 @@ exports.post_users = function (req, res) {
 exports.get_account = function (req, res) {
 	User.findById(req.user._id, '-password', function (err, user) {
 		if (err) {
-			response.respond(res, false, 500, 'Internal server error', null, err);
+			response.respond(res, false, 500, null, null, err);
 		} else {
 			response.respond(res, true, 200, 'Found user', user);
 		}
+	})
+};
+
+exports.post_account = function (req, res) {
+	User.findById(req.user._id, function (err, user) {
+		if (err) {
+			response.respond(res, false, 500, null, null, err);
+		} else {
+			for (var i in req.body.prefs) {
+				if (i != password) {
+					user[i] = prefs[i];
+				}
+			}
+		}
+		
 	})
 };
