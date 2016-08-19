de74190984bf25dad9c8b25e5039b2480d15e57b
Grady McDowell <gradyrmcd@gmail.com>
2016-08-19 13:55:09 -0400

    Add defaults for HTTP 500 error

--- controllers/response.js
+++ controllers/response.js
@@ -1,6 +1,14 @@
 exports.respond = function (res, success, code, message, data, err) {
 	if (err)
 		console.log(err);
+	if (code == 500) {
+		if (!success)
+			success = false;
+		if (!message)
+			message = 'Internal server error';
+		if (!data)
+			data = {'extended': 'A bug seems to have crawled into the server. This is not your fault.'};
+	}
 	res
 		.status(code)
 		.type('application/json')
@@ -8,7 +16,7 @@ exports.respond = function (res, success, code, message, data, err) {
 			'success': success,
 			'statusCode': code,
 			'message': message,
-			'data': data
+			'data': data || null
 		}))
 	;
 };
